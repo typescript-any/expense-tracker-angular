3adb45dfc9aba4b49c22107966eb3676628e6b9c
ayanonline <ayanghosh1389@gmail.com>
2025-03-14 16:33:26 +0530

    basic routing setup done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { PageNotFoundComponent } from './features/misc/page-not-found/page-not-found.component';
+import { SigninComponent } from './features/auth/signin/signin.component';
+import { SignupComponent } from './features/auth/signup/signup.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'signin', component: SigninComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
